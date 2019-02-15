65ce7d01c1225f5a01051c5dd82fd0809c131a65
Michael Krohn <michaelkrohn@zipcoders-MacBook-Pro-96.local>
2019-02-15 17:15:11 -0500

    Finished first half of lab

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,57 @@
 package com.zipcodewilmington.phonebook;
 
+import java.sql.SQLOutput;
+import java.util.*;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+
+    public TreeMap<String ,String > phoneBook = new TreeMap<String, String>();
+
+    public PhoneBook() {
+
+    }
+
+    public void add(String name, String phoneNumber) {
+        phoneBook.put(name, phoneNumber);
+    }
+
+    public String lookup(String name) {
+       return phoneBook.get(name);
+    }
+
+    public void remove(String name) {
+        phoneBook.remove(name);
+    }
+
+
+    public String reverseLookup(String phoneNumber) {
+        Set names = phoneBook.keySet();
+        Object[] namesArr = names.toArray();
+        String[] namesStringArr = Arrays.copyOf(namesArr, namesArr.length, String[].class);
+        for(String currentName : namesStringArr){
+            if(lookup(currentName) == phoneNumber){
+                return currentName;
+            }
+        }
+        return null;
+
+    }
+
+    public String display() {
+
+        StringBuilder output = new StringBuilder();
+        for(Map.Entry currentEntry : phoneBook.entrySet()){
+           output.append(currentEntry.getKey() + " ");
+           output.append(currentEntry.getValue());
+           output.append("\n");
+
+       }
+        System.out.println(output.toString());
+        return output.toString();
+
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,67 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+    private PhoneBook testPhoneBook;
+    String testName = "Mike";
+    String testPhoneNumber = "609-868-4500";
+    @Before
+    public void setup(){
+        this.testPhoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void addTest(){
+        //Given
+
+        //When
+        testPhoneBook.add(testName, testPhoneNumber);
+        String actual = testPhoneBook.lookup(testName);
+        //Then
+        Assert.assertEquals(testPhoneNumber, actual);
+
+    }
+
+    @Test
+    public void removeTest(){
+        //Given
+        testPhoneBook.add(testName,testPhoneNumber);
+        //When
+        testPhoneBook.remove(testName);
+        String actual = testPhoneBook.lookup(testName);
+        //Then
+        Assert.assertNull(actual);
+
+    }
+
+    @Test
+    public void reverseLookupTest(){
+        //Given
+        testPhoneBook.add(testName,testPhoneNumber);
+
+        //When
+        String expected = testPhoneBook.reverseLookup(testPhoneNumber);
+        //Then
+        Assert.assertEquals(expected, testName);
+
+    }
+
+    @Test
+    public void displayTest(){
+        //Given
+        testPhoneBook.add("Magneto","1-800-MAGNETBOI");
+        testPhoneBook.add(testName,testPhoneNumber);
+        String expected = "Magneto 1-800-MAGNETBOI\nMike 609-868-4500\n";
+        //When
+        String actual = testPhoneBook.display();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
